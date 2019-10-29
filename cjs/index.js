8bdd7a6c20f6eae7767d95438927dbde87924d74
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2019-10-29 12:13:22 +0100

    using push/pop to allow stacked traps

--- cjs/index.js
+++ cjs/index.js
@@ -3,18 +3,18 @@
 // a one liner functionality example
 // function mouse(trap) { return (mouse.trap = trap); }
 
-var mouse = (function (resolve, trap) {
-  function free() { trap = void 0; }
+var mouse = (function (trap) {
+  var pop = trap.pop.bind(trap);
+  var push = trap.push.bind(trap);
   return Object.defineProperty(
     function mouse(value) {
-      resolve.then(free);
-      return (trap = value);
+      return setTimeout(pop), push(value), value;
     },
     'trap',
-    {get: function () {
-      var value = trap;
-      return free(), value;
-    }}
+    {
+      get: pop,
+      set: push
+    }
   );
-}(Promise.resolve()));
+}([]));
 module.exports = mouse;
